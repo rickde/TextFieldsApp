346128621812bb583cc53ebfcec92f3a6cd5a368
Rick de Saussure <rickde@mac.com>
2017-05-11 16:18:19 -0600

    Updated View Controller to Stack View
    Signed-off-by: Rick de Saussure <rickde@mac.com>

--- TextFields/ViewController.swift
+++ TextFields/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var textField1: UITextField!
     @IBOutlet weak var textField2: UITextField!
     @IBOutlet weak var textField3: UITextField!
-    @IBOutlet weak var characterCountLabel: UILabel!
+
     
     // MARK: Text Field Delegate objects
     let emojiDelegate = EmojiTextFieldDelegate()
@@ -27,8 +27,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        // set the label to be hidden
-        self.characterCountLabel.isHidden = true
         
         // Set the three delegates
         self.textField1.delegate = emojiDelegate
@@ -40,16 +38,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 
-        // Figure out what the new text will be, if we return true
-        var newText = textField.text! as NSString
-        newText = newText.replacingCharacters(in: range, with: string) as NSString
         
-        // hide the label if the newText will be an empty string
-        self.characterCountLabel.isHidden = (newText.length == 0)
-        
-        // Write the length of newText into the label
-        self.characterCountLabel.text = String(newText.length)
-        
+
         // returning true gives the text field permission to change its text
         return true;
     }
