8338cbcf59f236b7a24cbeaed6f8940c72a8fd7e
Rick de Saussure <rickde@mac.com>
2017-05-12 09:06:30 -0600

    Added Lockable - Cash - ZipCode Delegate Files
    Signed-off-by: Rick de Saussure <rickde@mac.com>

---
+++ TextFields/CashTextFieldDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  CashTextFieldDelegate.swift
+//  TextFields
+//
+//  Created by Rick de Saussure on 5/12/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - CashTextFieldDelegate: NSObject, UITextFieldDelegate
+
+class CashTextFieldDelegate: NSObject, UITextFieldDelegate {
+}
---
+++ TextFields/ZipCodeTextFieldDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  ZipCodeTextFieldDelegate.swift
+//  TextFields
+//
+//  Created by Rick de Saussure on 5/12/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - ZipCodeTextFieldDelegate: NSObject, UITextFieldDelegate
+
+class ZipCodeTextFieldDelegate: NSObject, UITextFieldDelegate {
+}
